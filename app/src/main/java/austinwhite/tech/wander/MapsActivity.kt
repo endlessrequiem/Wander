f48883e9ae8e5bdec35415e3990af65268230c73
endlessrequiem <austin.j.white.96@gmail.com>
2020-07-16 14:39:28 -0700

    finished adding poi markers

--- app/src/main/java/austinwhite/tech/wander/MapsActivity.kt
+++ app/src/main/java/austinwhite/tech/wander/MapsActivity.kt
@@ -46,7 +46,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, zoomLevel))
         map.addMarker(MarkerOptions().position(homeLatLng))
         setMapLongClick(map)
-
+        setPoiClick(map)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -89,6 +89,17 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                             .title(getString(R.string.dropped_pin))
                             .snippet(snippet)
             )
+        }
+    }
+
+    private fun setPoiClick(map: GoogleMap) {
+        map.setOnPoiClickListener { poi ->
+            val poiMarker = map.addMarker(
+                    MarkerOptions()
+                            .position(poi.latLng)
+                            .title(poi.name)
+            )
+            poiMarker.showInfoWindow()
         }
     }
 }
